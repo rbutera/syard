8dcfe1739866a53d7d2a95c137a4b89a44f54f74
Rai <rai@rbutera.com>
2017-05-03 15:21:38 +0100

    scaffolded ScotlandYardTestLog

---
+++ src/test/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardTurnLog.java
@@ -0,0 +1,69 @@
+package uk.ac.bris.cs.scotlandyard.model;
+
+/**
+ * Created by rai on 03/05/2017.
+ */
+public class ScotlandYardTurnLog {
+    ArrayList<ScotlandYardTurn> mContents = new ArrayList<ScotlandYardTurn>();
+
+    public ArrayList<ScotlandYardTurn> getContents() {
+        return mContents;
+    }
+
+    public ScotlandYardTurnLog() {
+        System.out.println("        BEGIN LOG...    ");
+    }
+
+    /**
+     * finds the last DoubleMove in the log and returns it
+     * @return
+     */
+    public ScotlandYardTurn latestDouble() {
+        //TODO(rb): implement this!!
+        throw new IllegalArgumentException("implement latestDouble");
+    }
+
+    /**
+     * finds the latest entry in the log and returns it
+     * @return
+     */
+    public ScotlandYardTurn latest() {
+        //TODO(rb): implement this!!
+        throw new IllegalArgumentException("implement latestTurn!")
+    }
+
+    /**
+     * adds a new entry to the log
+     * @param input the new entry to be added
+     * @return
+     */
+    public int add(ScotlandYardTurn input) {
+        int index = 0;
+        if(true){
+            //TODO(rb): implement this!!
+            throw new IllegalArgumentException("implement SYTL.add!");
+        } else {
+            return index;
+        }
+    }
+
+    public ScotlandYardTurn nextColour() {
+        //TODO(rb): implement this!!
+        throw new IllegalArgumentException("implement SYTL.nextPlayer!");
+    }
+
+    public ScotlandYardTurn nextRound() {
+        //TODO(rb): implement this!!
+        throw new IllegalArgumentException("implement SYTL.nextRound!");
+    }
+
+    public ScotlandYardTurn nextTurn() {
+        //TODO(rb): implement this!!
+        throw new IllegalArgumentException("implement SYTL.nextTurn!");
+    }
+
+    public ArrayList<ScotlandYardTurn> getRound(int round){
+        //TODO(rb): implement this!!
+        throw new IllegalArgumentException("implement SYTL.getRound()");
+    }
+}
